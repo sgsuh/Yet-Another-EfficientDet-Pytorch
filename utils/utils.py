d68d8f17132a0d25deed7aab5ba9af2b09c99dc8
sgsuh <seukgyo.suh@gmail.com>
2025-11-15 04:53:14 +0000

    modify utils for ad custom dataset

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,9 @@
+"""
+Modify: 2025.11.15
+Author: SG.SUH
+PyTorch: 1.8
+Python: 3.8.5
+"""
 # Author: Zylo117
 
 import math
@@ -13,22 +19,20 @@ import webcolors
 from torch import nn
 from torch.nn.init import _calculate_fan_in_and_fan_out, _no_grad_normal_
 from torchvision.ops.boxes import batched_nms
+from torchvision.ops import nms
 
 from utils.sync_batchnorm import SynchronizedBatchNorm2d
 
 
 def invert_affine(metas: Union[float, list, tuple], preds):
+    scale = 4.21875
+
     for i in range(len(preds)):
-        if len(preds[i]['rois']) == 0:
+        if preds[i]['rois'].shape[0] == 0:
             continue
         else:
-            if metas is float:
-                preds[i]['rois'][:, [0, 2]] = preds[i]['rois'][:, [0, 2]] / metas
-                preds[i]['rois'][:, [1, 3]] = preds[i]['rois'][:, [1, 3]] / metas
-            else:
-                new_w, new_h, old_w, old_h, padding_w, padding_h = metas[i]
-                preds[i]['rois'][:, [0, 2]] = preds[i]['rois'][:, [0, 2]] / (new_w / old_w)
-                preds[i]['rois'][:, [1, 3]] = preds[i]['rois'][:, [1, 3]] / (new_h / old_h)
+            preds[i]['rois'][:, [0, 2]] = preds[i]['rois'][:, [0, 2]] *scale
+            preds[i]['rois'][:, [1, 3]] = (preds[i]['rois'][:, [1, 3]]) * scale
     return preds
 
 
@@ -78,23 +82,73 @@ def preprocess(*image_path, max_size=512, mean=(0.485, 0.456, 0.406), std=(0.229
 
 def preprocess_video(*frame_from_video, max_size=512, mean=(0.406, 0.456, 0.485), std=(0.225, 0.224, 0.229)):
     ori_imgs = frame_from_video
-    normalized_imgs = [(img[..., ::-1] / 255 - mean) / std for img in ori_imgs]
-    imgs_meta = [aspectaware_resize_padding(img, max_size, max_size,
-                                            means=None) for img in normalized_imgs]
-    framed_imgs = [img_meta[0] for img_meta in imgs_meta]
-    framed_metas = [img_meta[1:] for img_meta in imgs_meta]
+    normalized_imgs = [(img / 255 - mean) / std for img in ori_imgs]
 
-    return ori_imgs, framed_imgs, framed_metas
+    return normalized_imgs
 
+def postprocess_s(anchors, regression, classification, regressBoxes, clipBoxes, threshold, iou_threshold, tl_threshold=0.2):
+    transformed_anchors = regressBoxes(anchors, regression)
+    transformed_anchors = clipBoxes(transformed_anchors)
 
-def postprocess(x, anchors, regression, classification, regressBoxes, clipBoxes, threshold, iou_threshold):
+    tl_class = classification[:, 0] # w b
+    other_class = classification[:, 1:] # w b
+
+    scores = torch.max(classification, dim=2, keepdim=True)[0]
+
+    other_scores = torch.max(other_class, dim=2, keepdim=True)[0]
+
+    tl_scores_over_thresh = (tl_class > tl_threshold)[:] # wb
+    scores_over_thresh = (other_scores > threshold)[:, 0] # wb
+
+    scores_over_thresh = scores_over_thresh + tl_scores_over_thresh # wb
+
+    if torch.eq(scores_over_thresh.sum(),0):
+        return None, None, None
+
+    classification_per = classification[scores_over_thresh, ...].permute(1, 0) #ob
+    transformed_anchors_per = transformed_anchors[scores_over_thresh, ...] #ob
+    scores_per = scores[scores_over_thresh, ...] #ob
+    anchors_nms_idx = nms(transformed_anchors_per, scores_per[:, 0], iou_threshold=iou_threshold)
+
+    if anchors_nms_idx.shape[0] != 0:
+        scores_, classes_ = classification_per[:, anchors_nms_idx].max(dim=0)
+        boxes_ = transformed_anchors_per[anchors_nms_idx, :]
+
+        return boxes_, classes_, scores_
+
+    return None, None, None
+
+def postprocess(x, 
+                anchors, 
+                regression, 
+                classification, 
+                regressBoxes, 
+                clipBoxes, 
+                threshold, 
+                iou_threshold, 
+                tl_threshold=0.2):
     transformed_anchors = regressBoxes(anchors, regression)
     transformed_anchors = clipBoxes(transformed_anchors, x)
+
+    tl_class = classification[:, :, 0]
+    other_class = classification[:, :, 1:]
+
     scores = torch.max(classification, dim=2, keepdim=True)[0]
-    scores_over_thresh = (scores > threshold)[:, :, 0]
+    other_scores = torch.max(other_class, dim=2, keepdim=True)[0]
+
+    tl_scores_over_thresh = (tl_class > tl_threshold)[:, :]
+    scores_over_thresh = (other_scores > threshold)[:, :, 0]
+    scores_over_thresh = scores_over_thresh + tl_scores_over_thresh
+
     out = []
-    for i in range(x.shape[0]):
-        if scores_over_thresh[i].sum() == 0:
+    out_boxes = []
+    out_ids = []
+    out_scores = []
+
+    batch_size = 1
+
+    for i in range(batch_size):
+        if torch.eq(scores_over_thresh.sum(),0):
             out.append({
                 'rois': np.array(()),
                 'class_ids': np.array(()),
@@ -105,12 +159,10 @@ def postprocess(x, anchors, regression, classification, regressBoxes, clipBoxes,
         classification_per = classification[i, scores_over_thresh[i, :], ...].permute(1, 0)
         transformed_anchors_per = transformed_anchors[i, scores_over_thresh[i, :], ...]
         scores_per = scores[i, scores_over_thresh[i, :], ...]
-        scores_, classes_ = classification_per.max(dim=0)
-        anchors_nms_idx = batched_nms(transformed_anchors_per, scores_per[:, 0], classes_, iou_threshold=iou_threshold)
+        anchors_nms_idx = nms(transformed_anchors_per, scores_per[:, 0], iou_threshold=iou_threshold)
 
         if anchors_nms_idx.shape[0] != 0:
-            classes_ = classes_[anchors_nms_idx]
-            scores_ = scores_[anchors_nms_idx]
+            scores_, classes_ = classification_per[:, anchors_nms_idx].max(dim=0)
             boxes_ = transformed_anchors_per[anchors_nms_idx, :]
 
             out.append({
@@ -133,15 +185,15 @@ def display(preds, imgs, obj_list, imshow=True, imwrite=False):
         if len(preds[i]['rois']) == 0:
             continue
 
-        imgs[i] = imgs[i].copy()
-
         for j in range(len(preds[i]['rois'])):
             (x1, y1, x2, y2) = preds[i]['rois'][j].astype(np.int)
+            cv2.rectangle(imgs[i], (x1, y1), (x2, y2), (255, 255, 0), 2)
             obj = obj_list[preds[i]['class_ids'][j]]
             score = float(preds[i]['scores'][j])
 
-            plot_one_box(imgs[i], [x1, y1, x2, y2], label=obj, score=score,
-                         color=color_list[get_index_label(obj, obj_list)])
+            cv2.putText(imgs[i], '{}, {:.3f}'.format(obj, score),
+                        (x1, y1 + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                        (255, 255, 0), 1)
         if imshow:
             cv2.imshow('img', imgs[i])
             cv2.waitKey(0)
